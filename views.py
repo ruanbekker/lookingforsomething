92305a69b5d5abbb9b08a2e8538d435ecfd701ae
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-10-14 23:09:06 +0200

    add root view for demo

--- views.py
+++ views.py
@@ -1,1 +1,7 @@
+from flask import Blueprint, render_template
+from db import businesses
 
+root = Blueprint('root', __name__, template_folder='templates')
+@root.route('/')
+def main():
+    return render_template('index.html', company_name='Your Business Name')
