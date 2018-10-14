eb1a098d4cce7b59a49713f1a929e554bfbe869f
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-10-14 23:13:16 +0200

    add blueprints for views

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from views import root, business_pages
 
 app = Flask(__name__)
+app.register_blueprint(root)
+app.register_blueprint(business_pages)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, Debug=False)
