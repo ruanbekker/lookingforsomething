d70d9ffa6fdedad3ee9f06088041b1dbd24e10d3
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-10-14 22:53:33 +0200

    add db.py for testing

---
+++ db.py
@@ -0,0 +1,12 @@
+businesses = {
+    'page-1': {
+        'business_name': 'My Company Name',
+        'img_location': 'page-1-image.png',
+        'services': ['Service 1', 'Service 2', 'Service 3', 'Service 4', 'Service 5']
+    },
+    'page-2': {
+        'business_name': 'My Company Name 2',
+        'img_location': 'page-1-image.png',
+        'services': ['Service 1', 'Service 2', 'Service 3', 'Service 4']
+    }
+}
