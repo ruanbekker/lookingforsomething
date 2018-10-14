9a0a3b4c951756ce1b8aac8baf7a65c8fd973ccb
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-10-14 23:09:39 +0200

    add dynamic page routes

--- views.py
+++ views.py
@@ -5,3 +5,12 @@ root = Blueprint('root', __name__, template_folder='templates')
 @root.route('/')
 def main():
     return render_template('index.html', company_name='Your Business Name')
+
+business_pages = Blueprint('business_pages', __name__, template_folder='templates')
+@business_pages.route('/<page>', strict_slashes=False)
+def show(page):
+    if page in business:
+        company_name = businesses[page]['business_name']
+        services = businesses[page]['services']
+        img_src = businesses[page]['img_location']
+    return render_template('listings.html', company_name=company_name, services=services, img_src=img_src)
